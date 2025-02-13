8f199f9b90df900fbecc824c2c486f8781f38ed7
Zahra Ghasempour <ghasempour.zahra0@gmail.com>
2025-02-13 17:11:31 +0200

    add ui

--- app/src/main/java/com/example/currencyexchange/MainActivity.kt
+++ app/src/main/java/com/example/currencyexchange/MainActivity.kt
@@ -3,45 +3,94 @@ package com.example.currencyexchange
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.currencyexchange.ui.theme.CurrencyExchangeTheme
+import androidx.compose.ui.unit.dp
+import com.example.currencyexchange.viewmodel.CurrencyViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            CurrencyExchangeTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+         CurrencyExchangeApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun CurrencyExchangeApp(viewModel: CurrencyViewModel = viewModel()) {
+    var amount by remember { mutableStateOf("") }
+    var fromCurrency by remember { mutableStateOf("EUR") }
+    var toCurrency by remember { mutableStateOf("USD") }
+    var message by remember { mutableStateOf("") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CurrencyExchangeTheme {
-        Greeting("World")
+    val currencies = viewModel.rates.keys.toList()
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("Your Balance : ${viewModel.userBalance}")
+        OutlinedTextField(
+            value = amount,
+            onValueChange = { amount = it},
+            label = { Text("Enter Amount")}
+        )
+        DropdownMenuPicker("From Currency", currencies, fromCurrency){ fromCurrency = it}
+        DropdownMenuPicker("To Currency", currencies, toCurrency){ toCurrency = it}
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(onClick = {
+            val amountDouble = amount.toDoubleOrNull()
+            message = if (amountDouble != null){
+                viewModel.convertCurrency(amountDouble,fromCurrency,toCurrency)
+            }else {
+                "Invalid amount"
+            }
+        },
+            modifier = Modifier.height(10.dp)
+        ) {
+            Text("Convert")
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(text = message, style = MaterialTheme.typography.bodySmall)
     }
 }
+
+@Composable
+fun DropdownMenuPicker(label: String,
+                       options: List<String>,
+                       selectedOption: String,
+                       onOptionSelected: (String) -> Unit)
+{
+    var expanded by remember { mutableStateOf(false) }
+
+    Box{
+        TextButton(onClick = { expanded = true }) {
+            Text(selectedOption)
+        }
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            options.forEach { option ->
+                DropdownMenuItem(text = {Text(text = option)} , onClick = {
+                    onOptionSelected(option)
+                    expanded = false
+                })
+            }
+        }
+    }
+}
+
