06f8e6fcf6aa1ebfb46154ca80b76f2caca1a6aa
Zahra Ghasempour <ghasempour.zahra0@gmail.com>
2025-05-07 10:05:21 +0300

    add icon

--- app/src/main/java/com/example/currencyexchange/MainActivity.kt
+++ app/src/main/java/com/example/currencyexchange/MainActivity.kt
@@ -3,22 +3,30 @@ package com.example.currencyexchange
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.example.currencyexchange.viewmodel.CurrencyViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -31,24 +39,64 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
 @Composable
 fun CurrencyExchangeApp(viewModel: CurrencyViewModel = viewModel()) {
     var amount by remember { mutableStateOf("") }
     var fromCurrency by remember { mutableStateOf("EUR") }
     var toCurrency by remember { mutableStateOf("USD") }
     var message by remember { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }
 
     val currencies = viewModel.rates.keys.toList()
-
+    val context = LocalContext.current
     Column(modifier = Modifier.padding(16.dp)) {
-        Text("Your Balance : ${viewModel.userBalance}")
-        OutlinedTextField(
-            value = amount,
-            onValueChange = { amount = it},
-            label = { Text("Enter Amount")}
-        )
-        DropdownMenuPicker("From Currency", currencies, fromCurrency){ fromCurrency = it}
-        DropdownMenuPicker("To Currency", currencies, toCurrency){ toCurrency = it}
+        Text("MY BALANCES")
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = viewModel.userBalance.entries.joinToString("     ") { "${it.key}=${it.value}" }.removeSurrounding("{","}"))
+        Spacer(modifier = Modifier.height(20.dp))
+        Row (Modifier.fillMaxWidth()){
+            Image(painter = painterResource(id = R.drawable.ic_sell), contentDescription ="sell icon" ,
+                modifier = Modifier.padding(top = 16.dp))
+            Text(text = "Sell",
+                modifier = Modifier
+                    .width(150.dp)
+                    .padding(top = 20.dp))
+            TextField(
+                value = amount,
+                onValueChange = { amount = it},
+                label = { Text("")},
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color.Transparent, // Background when not focused
+                    focusedContainerColor = Color.Transparent // Background when focused
+                ),
+                modifier = Modifier
+                    .width(100.dp)
+
+            )
+            DropdownMenuPicker("From Currency", currencies, fromCurrency){ fromCurrency = it}
+        }
+        Row(Modifier.fillMaxWidth()) {
+            Image(painter = painterResource(id = R.drawable.ic_receive), contentDescription ="receive icon" ,
+                modifier = Modifier.padding(top = 16.dp))
+            Text(text = "Receive",
+                modifier = Modifier
+                    .width(150.dp)
+                    .padding(top = 20.dp))
+            TextField(
+                value = "",
+                onValueChange = { amount = it},
+                label = { Text("")},colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color.Transparent, // Background when not focused
+                    focusedContainerColor = Color.Transparent // Background when focused
+                ),
+                modifier = Modifier
+                    .width(100.dp)
+
+            )
+
+            DropdownMenuPicker("To Currency", currencies, toCurrency){ toCurrency = it }
+        }
 
         Spacer(modifier = Modifier.height(10.dp))
 
@@ -59,15 +107,16 @@ fun CurrencyExchangeApp(viewModel: CurrencyViewModel = viewModel()) {
             }else {
                 "Invalid amount"
             }
-        },
-            modifier = Modifier.height(10.dp)
+            showDialog =true
+        }, modifier = Modifier.align(Alignment.CenterHorizontally)
+            .width(200.dp)
         ) {
             Text("Convert")
         }
-
+        CustomDialog(showDialog = showDialog, onDismiss = {showDialog = false}, message)
         Spacer(modifier = Modifier.height(10.dp))
 
-        Text(text = message, style = MaterialTheme.typography.bodySmall)
+       // Text(text = message, style = MaterialTheme.typography.bodySmall)
     }
 }
 
@@ -79,7 +128,7 @@ fun DropdownMenuPicker(label: String,
 {
     var expanded by remember { mutableStateOf(false) }
 
-    Box{
+    Box(modifier = Modifier.padding(top = 18.dp)){
         TextButton(onClick = { expanded = true }) {
             Text(selectedOption)
         }
@@ -89,6 +138,30 @@ fun DropdownMenuPicker(label: String,
                     onOptionSelected(option)
                     expanded = false
                 })
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomDialog(showDialog: Boolean, onDismiss: () -> Unit, message:String) {
+    if (showDialog) {
+        Dialog(onDismissRequest = onDismiss) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White, shape = RoundedCornerShape(16.dp))
+                    .padding(16.dp)
+            ) {
+                Column {
+                    Text(text = "Currency Converted", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = message)
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = onDismiss) {
+                        Text("Done")
+                    }
+                }
             }
         }
     }
